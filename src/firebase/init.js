f9467b7765ca759bb333b9b93ef3e2e51570e564
Symon Gonthiez <seymonx@gmail.com>
2019-11-30 02:28:33 +0100

    Firestore Setup - Welcome Screen - Passing Props Via Routes

---
+++ src/firebase/init.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDHHk4zicVjTjDsIWvnTbrc9sUxLVxgr10",
+    authDomain: "vue-live-chat-7aea8.firebaseapp.com",
+    databaseURL: "https://vue-live-chat-7aea8.firebaseio.com",
+    projectId: "vue-live-chat-7aea8",
+    storageBucket: "vue-live-chat-7aea8.appspot.com",
+    messagingSenderId: "926641928037",
+    appId: "1:926641928037:web:2bbe0b79e32ca6a6f8938c",
+    measurementId: "G-XDG3XPR9ZF"
+  };
+
+const firebase = firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true})
+//firebase.analytics();
+
+export default firebaseApp.firestore()
