15ae860a4de2dfeec224a6408bad96211081e7a7
Symon Gonthiez <seymonx@gmail.com>
2019-12-01 00:40:24 +0100

    Route Guard

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,11 @@ export default new Router({
       path: '/chat',
       name: 'Chat',
       component: Chat,
-      props: true
+      props: true,
+      beforeEnter: (to, from, next) => {
+        console.log(to.params.name)
+        next()
+      } 
     }
   ]
 })
